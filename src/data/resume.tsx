989161735bcfc89b6dd301749592873e5ff02bfe
Aishwarya-2311 <276198373+Aishwarya-2311@users.noreply.github.com>
2026-04-15 21:05:40 +0530

    Remove video links from resume.tsx
    Removed video links from resume entries.

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -172,9 +172,7 @@ export const DATA = {
       },
       ],
       image: "",
-      video:
-      "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
-    },
+      },
     {
       title: "Augmented Reality Home Decor Application",
       href: "#",
@@ -193,9 +191,7 @@ export const DATA = {
       },
       ],
       image: "",
-      video:
-      "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
-    },
+      },
   ],
   /*
   hackathons: [
